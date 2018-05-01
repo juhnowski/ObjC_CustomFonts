d95a5286faa853d2e267f1e395aff26140c6e3e0
Juhnowski <juhnowski@gmail.com>
2018-05-01 14:00:56 +0300

    Initial Commit

---
+++ CistomFonts1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CistomFonts1
+//
+//  Created by SBT on 01/05/2018.
+//  Copyright © 2018 SBT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
